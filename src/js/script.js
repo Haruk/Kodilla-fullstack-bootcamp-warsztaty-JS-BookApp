484fd3b1f0b1fd9ccdc2813cc4c908efb77a47c8
Aleksandra <haruka.lex@gmail.com>
2023-01-31 20:05:12 +0100

    Add/remove books on fav list

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,7 @@
       },
       containerOf: {
         books: '.books-list',
+        images: '.books-list .book__image',
       },
     };
   
@@ -16,20 +17,38 @@
       books: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
     };
   
+    const favoriteBooks = [];
+  
     function render() {
-      //console.log(document.querySelector(select.templateOf.books));
+      
       for(let book of dataSource.books){
         const bookHTML = templates.books(book);
-        console.log('bookHTML',bookHTML);
-  
         const bookDOM = utils.createDOMFromHTML(bookHTML);
-        console.log('bookDOM',bookDOM);
-  
         const bookContainer = document.querySelector(select.containerOf.books);
-        console.log('bookContainer',bookContainer);
+        
         bookContainer.appendChild(bookDOM);
       }
     }
   
+    function initActions() {
+  
+      const images = document.querySelectorAll(select.containerOf.images);
+      for (let image of images) {
+        image.addEventListener('dblclick', function (event) {
+          event.preventDefault();
+          image.classList.toggle('favorite');
+
+          let imageID = image.getAttribute('data-id');
+
+          if(!favoriteBooks.includes(imageID)){
+            favoriteBooks.push(imageID);
+          }else if(favoriteBooks.includes(imageID)){
+            favoriteBooks.splice(favoriteBooks.indexOf(imageID, 1));
+          } 
+        });
+      }
+    }
+  
     render();
+    initActions();
   }
